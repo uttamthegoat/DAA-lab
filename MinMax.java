5f0605c016bf327139d0f0538026bd264fe9bff4
uttamthegoat <118157434+uttamthegoat@users.noreply.github.com>
2023-07-13 11:56:31 +0530

    Add files via upload

---
+++ MinMax.java
@@ -0,0 +1,20 @@
+package sample;
+import java.util.*;
+
+public class MinMax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] a = new int[size];
+		
+		for(int i=0;i<size;i++) {
+			a[i]=sc.nextInt();
+		}
+		
+//		logic of min and max
+		min
+	}
+
+}
